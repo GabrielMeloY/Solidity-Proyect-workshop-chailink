6589bc9e41ce636c09e1cf76e78c5eecc102f49b
Luis Lucena <lucena@puller.mx>
2022-09-09 00:22:38 -0400

    edit hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,16 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.14",
+  paths: {
+    artifacts: "./src/backend/artifacts",
+    sources: "./src/backend/contracts",
+    cache: "./src/backend/cache",
+    tests: "./src/backend/test"
+  },
+  networks: {
+    hardhat: {
+    chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
+    },
+  },
 };
